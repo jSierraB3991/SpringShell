30cd8059dfd7437eba5e27d898dbe5079be970af
Juan David Sierra <judas3991@gmail.com>
2022-09-08 22:17:09 -0500

    create project for JS Shell

---
+++ jSShell/src/main/java/com/example/jSShell/JSShellApplication.java
@@ -0,0 +1,13 @@
+package com.example.jSShell;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JSShellApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JSShellApplication.class, args);
+	}
+
+}
